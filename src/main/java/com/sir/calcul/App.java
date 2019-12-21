81e290795f673eb257d747ca4218c5d75687ccb9
dessteyu <bayembye10@gmail.com>
2019-12-21 23:26:46 +0000

    commit first tests

--- src/main/java/com/sir/calcul/App.java
+++ src/main/java/com/sir/calcul/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Faire une addtion de 10 et de 20" );
+        System.out.println("la somme de 10 et de 20: " + Calcul.addition(10,20));
+        System.out.println("Faire la multiplication de 10 et de 20");
+        System.out.println("la somme de 10 et de 20: " + Calcul.multiplication(10,20));
     }
 }
--- src/test/java/com/sir/calcul/AppTest.java
+++ src/test/java/com/sir/calcul/AppTest.java
@@ -17,4 +17,23 @@ public class AppTest
     {
         assertTrue( true );
     }
+    
+    @Test 
+    public void verifieAddition(){
+        
+    }
+    
+    @Test
+    public void verifieMultiplication(){
+        int resultat = 10 * 20;
+        assertTrue("la multiplication de 10 et 20 doit etre egal a 200", Calcul.multiplication(10, 20) == resultat);
+        resultat = 1 * 10;
+        assertTrue("la multiplication de 10 et 1 doit etre egal a00", Calcul.multiplication(1, 20) == resultat);
+        resultat = 65 * 7;
+        assertTrue("la multiplication de 10 et 1 doit etre egal a 200", Calcul.multiplication(65, 7) == resultat);
+//        a et b sont negatifs
+        resultat = (-4) * (-7);
+        assertTrue("a * b doit etre possitifs est egal a 34 ", Calcul.multiplication(-4, -7) == resultat);
+    }
+    
 }
